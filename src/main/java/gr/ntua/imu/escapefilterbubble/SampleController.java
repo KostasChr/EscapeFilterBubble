db6f2433b73b44aac49f36ce806688b5a519b207
Kostas Christidis <costas.x@gmail.com>
2014-02-10 18:07:54 +0200

    First commit

---
+++ src/main/java/gr/ntua/imu/escapefilterbubble/SampleController.java
@@ -0,0 +1,16 @@
+package gr.ntua.imu.escapefilterbubble;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/")
+public class SampleController {
+	@RequestMapping(method = RequestMethod.GET)
+	public String printWelcome(ModelMap model) {
+		model.addAttribute("message", "Hello world- that is my app!");
+		return "test";
+	}
+}
